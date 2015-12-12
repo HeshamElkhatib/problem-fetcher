7b6b2245ce4ed3800d0e2b2f38ac20daaada86bd
Hesham Abdelhay <hesham.elkhatib@hotmail.com>
2015-12-12 18:32:45 +0200

    Create Problem scrapy item for the scraped online judges problem Create all the required attributes

--- ojs_crawlers/ojs_crawlers/items.py
+++ ojs_crawlers/ojs_crawlers/items.py
@@ -8,7 +8,26 @@
 import scrapy
 
 
-class OjsCrawlersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Problem(scrapy.Item):
+    # its remote_id on the online judge
+    remote_id = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    # the problem description
+    statement = scrapy.Field()
+    # the date of creation on the online judge
+    creation_date = scrapy.Field()
+    # indicating its categories (dynamic programming, graph theory, .. etc)
+    tags = scrapy.Field()
+    # online judge official name
+    online_judge = scrapy.Field()
+    # some online judges explicitly define each problem difficulty
+    difficulty = scrapy.Field()
+    # number of users who solved the problem
+    solved_count = scrapy.Field()
+    # number of wrong tries
+    wrong_tries = scrapy.Field()
+    # problem level (in case the online judge explicitly specifying it difficulty level)
+    # it might be the problems point
+    level = scrapy.Field()
+
